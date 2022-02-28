7e2ac95a3acab392f3c7c8cc28809fa951b020aa
hoon <hoonnotes@naver.com>
2022-02-28 10:40:33 +0900

    chore: cors, bodyparser, debugging 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
+import * as mongoose from 'mongoose';
 import { AppModule } from './app.module';
+import { __PROD__ } from './utils/constants';
+
+const allowedOrigins = ['http://localhost:3000'];
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const logger = new Logger();
+
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+    cors: {
+      origin: allowedOrigins,
+      credentials: true,
+    },
+  });
+
+  if (!__PROD__) {
+    app.use(morgan('dev'));
+    mongoose.set('debug', { shell: true });
+  }
+
+  const PORT = process.env.PORT || 8080;
+
+  await app.listen(PORT);
+
+  logger.log(`[Application]: ${PORT}에서 서버 실행중`);
 }
 bootstrap();
