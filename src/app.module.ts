1f45c68f982b33ab5c14be3b57279a7beda2deff
hoon <hoonnotes@naver.com>
2022-02-28 10:18:56 +0900

    Feat(Database): 서버 앱에 데이터베이스 연결
    환경변수 활용을 위한 config module 패키지 설치
    데이터베이스 orm 목적의 nestjs/mongoose 설치
    데이터베이스 모듈 생성
    - prod, marketing_prod, marketing_dev 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number(),
+        NODE_ENV: Joi.string(),
+        DB_PROD: Joi.string(),
+        DB_MARKETING: Joi.string(),
+        DB_MARKETING_DEV: Joi.string(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DB_PROD'),
+      }),
+      connectionName: 'PROD',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DB_MARKETING'),
+      }),
+      connectionName: 'MARKETING_PROD',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DB_MARKETING_DEV'),
+      }),
+      connectionName: 'MARKETING_DEV',
+    }),
+  ],
+})
+export class DatabaseModule {}
