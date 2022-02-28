ef8a275f3fb79cfbfcf619b5b1d8f54445f30bb9
hoon <hoonnotes@naver.com>
2022-02-28 14:10:45 +0900

    feat(constant): 지금 시간을 위한 상수

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
+import { DateTime } from 'luxon';
 export const __PROD__ = process.env.NODE_ENV === 'production';
 export const __MARKETING_DB__ = __PROD__ ? 'DB_MARKETING' : 'DB_MARKETING_DEV';
+export const __NOW__ = DateTime.now().setZone('Asia/Seoul');
