3b4ac40382221b83391d265072a48375b8ab4dd7
hoon <hoonnotes@naver.com>
2022-02-28 11:15:00 +0900

    feat(admin-user): 어드민 유저 조회 및 생성로직 구현

---
+++ src/admin-users/admin-users.service.ts
@@ -0,0 +1,44 @@
+import { Model } from 'mongoose';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { AdminUser, AdminUserDocument } from './admin-user.schema';
+import { AdminUserGroup, AdminUserRole } from './admin-user.enum';
+
+@Injectable()
+export class AdminUsersService {
+  constructor(
+    @InjectModel(AdminUser.name)
+    private readonly adminUser: Model<AdminUserDocument>,
+  ) {}
+
+  public async getUserByEmail(email: string) {
+    const user = await this.adminUser.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException(`${email}는 등록되지 않은 이메일입니다`);
+    }
+
+    return user;
+  }
+
+  public async getUserById(id: string) {
+    const user = await this.adminUser.findById(id);
+
+    if (!id) {
+      throw new NotFoundException(
+        `${id}에 해당하는 어드민 사용자가 존재하지 않습니다`,
+      );
+    }
+
+    return user;
+  }
+
+  public createUserWithGoogle(email: string, name: string) {
+    return this.adminUser.create({
+      email,
+      name,
+      role: AdminUserRole.MEMBER,
+      group: AdminUserGroup.CORP_REG,
+    });
+  }
+}
