719755af432d807621daa804f8c48d539a4a0118
hoon <hoonnotes@naver.com>
2022-02-28 11:14:35 +0900

    feat(admin-user): 어드민 유저 그룹 / 롤 이넘 구현

---
+++ src/admin-users/admin-user.enum.ts
@@ -0,0 +1,14 @@
+export enum AdminUserRole {
+  MEMBER = 'MEMBER',
+  LEADER = 'LEADER',
+  MANAGER = 'MANAGER',
+}
+
+export enum AdminUserGroup {
+  CORP_REG = 'CORP_REG',
+  MARKETING = 'MARKETING',
+  DND = 'DND',
+  SALES = 'SALES',
+  INHERIT = 'INHERIT',
+  OPERATION = 'OPERATION',
+}
